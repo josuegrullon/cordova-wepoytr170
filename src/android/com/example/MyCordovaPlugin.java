b65486eb94237325a2a742a49ae47e2a6420af22
Josue Grullon <josuegrullon@gmail.com>
2017-09-07 22:48:38 -0400

    First commit

--- src/android/com/example/MyCordovaPlugin.java
+++ src/android/com/example/MyCordovaPlugin.java
@@ -11,24 +11,46 @@ import org.apache.cordova.PluginResult.Status;
 import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
-
+import woyou.aidlservice.jiuiv5.ICallback;
+import woyou.aidlservice.jiuiv5.IWoyouService;
+//import android.content.*;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.util.Log;
+import android.content.ComponentName;
+import android.content.ServiceConnection;
+import android.app.Activity;
 
 import java.util.Date;
 
-public class MyCordovaPlugin extends CordovaPlugin {
-  private static final String TAG = "MyCordovaPlugin";
+public class wepoyPrint extends CordovaPlugin {
+  private static final String TAG = "wepoyPrint";
 
   public void initialize(CordovaInterface cordova, CordovaWebView webView) {
     super.initialize(cordova, webView);
 
-    Log.d(TAG, "Initializing MyCordovaPlugin");
+      Intent intent = new Intent();
+      intent.setPackage("woyou.aidlservice.jiuiv5");
+      intent.setAction("woyou.aidlservice.jiuiv5.IWoyouService");
+      webView.getContext().startService(intent);//启动打印服务
+      webView.getContext().bindService(intent, connService, Context.BIND_AUTO_CREATE);
+    Log.d(TAG, "Initializing wepoyPrint");
   }
 
   public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
     if(action.equals("echo")) {
       String phrase = args.getString(0);
       // Echo back the first argument
+        try {
+            woyouService.printerInit(callback);
+            woyouService.printText("Hola sergio from matrices :) \n Que lucha por Dios \n\n\n", callback);
+            woyouService.lineWrap(4, callback);
+            woyouService.printOriginalText(phrase, callback);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
       Log.d(TAG, phrase);
     } else if(action.equals("getDate")) {
       // An example of returning data back to the web layer
@@ -37,5 +59,36 @@ public class MyCordovaPlugin extends CordovaPlugin {
     }
     return true;
   }
+    private IWoyouService woyouService;
+
+    private ServiceConnection connService = new ServiceConnection() {
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            woyouService = null;
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            woyouService = IWoyouService.Stub.asInterface(service);
+        }
+    };
+
+
+    ICallback callback = new ICallback.Stub() {
+
+        @Override
+        public void onRunResult(boolean success) throws RemoteException {
+        }
+
+        @Override
+        public void onReturnString(final String value) throws RemoteException {
+        }
+
+        @Override
+        public void onRaiseException(int code, final String msg)
+                throws RemoteException {
+        }
+    };
 
 }
